c204ccb0ca46628ad40d97b332b30f13f9b4c2ae
Administrator <2440642389@qq.com>
2020-04-21 09:23:01 +0800

    firstcode

---
+++ App.java
@@ -0,0 +1,14 @@
+package mytest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        new Newtest().test();
+    }
+}
---
+++ Newtest.java
@@ -0,0 +1,11 @@
+package mytest;
+
+/**
+ * Created by Administrator on 2020/4/21 0021.
+ */
+
+class Newtest {
+    public void test(){
+        System.out.print("我就是力量的化身！");
+    }
+}
